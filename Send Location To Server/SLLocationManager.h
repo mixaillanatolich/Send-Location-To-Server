583850730408f63bf5987df639095adc3728dd84
Mixail Muravev <mixaillanatolich@gmail.com>
2013-06-10 21:55:49 +0400

    create location manager V1.0

---
+++ Send Location To Server/SLLocationManager.h
@@ -0,0 +1,25 @@
+//
+//  SLLocationManager.h
+//  Send Location To Server
+//
+//  Created by Mixaill Anatolich on 6/10/13.
+//  Copyright (c) 2013 Mixaill Anatolich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+typedef void(^locationHandler)(CLLocation *location);
+
+@interface SLLocationManager : NSObject <CLLocationManagerDelegate>
+
++ (SLLocationManager*)sharedManager;
+
+@property (nonatomic, copy) locationHandler locationUpdatedInForeground;
+@property (nonatomic, copy) locationHandler locationUpdatedInBackground;
+
+- (void)startUpdatingLocation;
+- (void)stopUpdatingLocation;
+- (void)endBackgroundTask;
+
+@end
---
+++ Send Location To Server/SLLocationManager.m
@@ -0,0 +1,111 @@
+//
+//  SLLocationManager.m
+//  Send Location To Server
+//
+//  Created by Mixaill Anatolich on 6/10/13.
+//  Copyright (c) 2013 Mixaill Anatolich. All rights reserved.
+//
+
+#import "SLLocationManager.h"
+
+static CGFloat const kMinUpdateDistance = 10.f;
+static NSTimeInterval const kMinUpdateTime = 90.f;
+static NSTimeInterval const kMaxTimeToLive = 30.f;
+
+@interface SLLocationManager () {
+    UIBackgroundTaskIdentifier bgTask;
+}
+
+@property (nonatomic, strong) CLLocationManager *locationManager;
+
+@end
+
+@implementation SLLocationManager
+
++ (SLLocationManager*)sharedManager {
+    static SLLocationManager* sharedSLLocationManagerInstance = nil;
+    static dispatch_once_t predicate;
+    dispatch_once( &predicate, ^{
+        sharedSLLocationManagerInstance = [[self alloc] init];
+    } );
+    return sharedSLLocationManagerInstance;
+}
+
+#pragma mark - NSObject
+
+- (id)init {
+    if (self = [super init]) {
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidBecomeActive) name:UIApplicationDidBecomeActiveNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidEnterBackground) name:    UIApplicationDidEnterBackgroundNotification object:nil];
+        self.locationManager = [[CLLocationManager alloc] init];
+        self.locationManager.delegate = self;
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    self.locationManager.delegate = nil;
+}
+
+#pragma mark - Notification handlers
+
+- (void)applicationDidBecomeActive {
+    [self.locationManager stopMonitoringSignificantLocationChanges];
+    [self.locationManager startUpdatingLocation];
+}
+
+- (void)applicationDidEnterBackground {
+    [self.locationManager stopUpdatingLocation];
+    [self.locationManager startMonitoringSignificantLocationChanges];
+}
+
+#pragma mark - Public
+
+- (void)startUpdatingLocation {
+    [self stopUpdatingLocation];
+    [self isInBackground] ? [self.locationManager startMonitoringSignificantLocationChanges] : [self.locationManager startUpdatingLocation];
+}
+
+- (void)stopUpdatingLocation {
+    [self.locationManager stopUpdatingLocation];
+    [self.locationManager stopMonitoringSignificantLocationChanges];
+}
+
+- (void)endBackgroundTask {
+    if (bgTask != UIBackgroundTaskInvalid) {
+        [[UIApplication sharedApplication] endBackgroundTask:bgTask];
+        bgTask = UIBackgroundTaskInvalid;
+    }
+}
+#pragma mark - Private
+
+- (BOOL)isInBackground {
+    return [UIApplication sharedApplication].applicationState == UIApplicationStateBackground;
+}
+
+#pragma mark - CLLocationManager Delegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+    if (oldLocation && ([newLocation.timestamp timeIntervalSinceDate:oldLocation.timestamp] < kMinUpdateTime ||
+                        [newLocation distanceFromLocation:oldLocation] < kMinUpdateDistance)) {
+        return;
+    }
+    
+    if ([self isInBackground]) {
+        if (self.locationUpdatedInBackground) {
+            bgTask = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler: ^{
+                [[UIApplication sharedApplication] endBackgroundTask:bgTask];
+            }];
+            
+            self.locationUpdatedInBackground(newLocation);
+            [self endBackgroundTask];
+        }
+    } else {
+        if (self.locationUpdatedInForeground) {
+            self.locationUpdatedInForeground(newLocation);
+        }
+    }
+}
+
+@end
