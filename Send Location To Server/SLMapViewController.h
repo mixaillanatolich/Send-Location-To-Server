612550e34d73c3da300561c690f1ee0143c13bd4
Mixail Muravev <mixaillanatolich@gmail.com>
2013-06-19 00:02:10 +0400

    add icons on tabbar

---
+++ Send Location To Server/SLMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLMapViewController.h
+//  Send Location To Server
+//
+//  Created by Mixaill Anatolich on 6/18/13.
+//  Copyright (c) 2013 Mixaill Anatolich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLMapViewController : UIViewController
+
+@end
---
+++ Send Location To Server/SLMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  SLMapViewController.m
+//  Send Location To Server
+//
+//  Created by Mixaill Anatolich on 6/18/13.
+//  Copyright (c) 2013 Mixaill Anatolich. All rights reserved.
+//
+
+#import "SLMapViewController.h"
+
+@interface SLMapViewController ()
+
+@end
+
+@implementation SLMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
